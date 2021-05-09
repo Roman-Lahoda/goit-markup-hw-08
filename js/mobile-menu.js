d88eacd8d4532865fe62cfaa3860e49e1bbe1376
Roman-Lahoda <lagoda9515@gmail.com>
2021-05-09 11:05:28 +0300

    fixed mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,16 @@
 ;(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]")
+  const menuBtn2Ref = document.querySelector("[data-menu-button2]")
   const mobileMenuRef = document.querySelector("[data-menu]")
   menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+    document.body.classList.toggle("modal-open")
+    menuBtnRef.classList.toggle("is-open")
+    menuBtnRef.setAttribute("aria-expanded", !expanded)
+    mobileMenuRef.classList.toggle("is-open")
+  })
+
+  menuBtn2Ref.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
     document.body.classList.toggle("modal-open")
     menuBtnRef.classList.toggle("is-open")
